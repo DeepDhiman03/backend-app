0cb5858c91ab4985fb6df54fe0e2d8a2a7f5782f
DeepDhiman03 <118919161+DeepDhiman03@users.noreply.github.com>
2024-12-08 21:31:08 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const startTime = Date.now();
+app.get('/status', (req, res) => {
+  res.json({
+    status: 'OK',
+    uptime: \`\${(Date.now() - startTime) / 1000}s\`
+  });
+});
+
+app.listen(80, () => console.log('App running on port 80'));
