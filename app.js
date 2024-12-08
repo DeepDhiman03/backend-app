059616266c88c5529a84a5a0bec6bd1269203cfa
DeepDhiman03 <118919161+DeepDhiman03@users.noreply.github.com>
2024-12-08 21:38:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const startTime = Date.now();
+
 app.get('/status', (req, res) => {
   res.json({
     status: 'OK',
-    uptime: \`\${(Date.now() - startTime) / 1000}s\`
+    uptime: `${(Date.now() - startTime) / 1000}s`
   });
 });
 
